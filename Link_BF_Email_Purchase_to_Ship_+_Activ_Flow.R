a41bc07f4c7769f5758eec49c261e1e89635fd52
matthijs-kuijt <matthijs.kuijt@ring.com>
2019-12-17 15:35:04 +0100

    Initial Commit

---
+++ Link_BF_Email_Purchase_to_Ship_+_Activ_Flow.R
@@ -0,0 +1,10 @@
+library(DBI)
+library(RPostgreSQL)
+library(xlsx)
+library(tidyverse)
+
+source('C:/Users/kuijt/Documents/R/Connection/R_to_MSSQL_Conn_v(prod).R')
+
+q1 = 'SELECT TOP(10) * FROM Ring_EU_Masterdata.dbo.Master_Products'
+
+A = dbGetQuery(msconn_rsql,q1)
